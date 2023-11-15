7d12fc4c42276bf2d7acfbf47ec6215de6adc5b4
Patrik Malvenius <malvenius@gmail.com>
2023-11-15 15:38:49 +0100

    basic setup for instanciated geoms

--- container_3dtiles/pg2b3dm/init.sh
+++ container_3dtiles/pg2b3dm/init.sh
@@ -1,10 +1,9 @@
 # this is a sample command to be executed from the pg2b3dm-container after all is initialized
+# pg2b3dm -h pg_db -U magic -c geom_triangle -t batiment -d magic -a id,hauteur,nature,usage1,usage2 --use_implicit_tiling 'false' --default_color '#b7b9bd' --add_outlines 'true' 
 
-pg2b3dm -h pg_db -U magic -c geom_triangle -t batiment -d magic -a id,hauteur,nature,usage1,usage2 --use_implicit_tiling 'false' --default_color '#b7b9bd' --add_outlines 'true' 
+i3dm.export -c "Host=pg_db;Username=magic;Password=magic;Database=magic;Port=5432" -t public.arbres
 
-#tesselate_building -h pg_db -U magic -d magic -f cesium -t bati_sauv -i wkb_geometry -o geom_triangle --heightcolumn hauteur --idcolumn ogc_fid --stylecolumn style --shaderscolumn shaders
 
-#tesselate building sets all buildings to z=0 -> works like shit for me since i have all heights in pseudo-orthometric.
-# can not set st_translate to add z_sol_min after tesselated because that warps the buildings (geodesic mindfuck, z is perpendicular to santas beard)
-# that is why after going bald i settled on some sql instead, see init.sql
+
+
 
